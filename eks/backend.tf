7f386d9513497f23ea52f7abebeb72596a8cb9e2
cloud_user <cloud-user@eks-acg.com>
2024-10-18 18:49:44 +0200

    init

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.3"
+  # required_version = "~> 1.9.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-ews-baket1-vladosur"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
